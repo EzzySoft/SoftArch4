cfefb8fb53722f85d7d0a516dbeb5c0ecc004891
Dmitry Zvidrin <dvzvidrin48@gmail.com>
2024-10-03 16:42:06 +0300

    FishEye Filter

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+import cv2
+
 from capture import VideoCapturePipe
-from filters import MirrorFilterPipe
+from filters import MirrorFilterPipe, FishEyeFilterPipe
 from display import DisplayPipe
+
 
 def video_processing_pipeline():
     capture_pipe = VideoCapturePipe()
     mirror_filter = MirrorFilterPipe()
+    fisheye_filter = FishEyeFilterPipe()
+
     mirrored_display_pipe = DisplayPipe('Mirrored Video')
+    fisheye_display_pipe = DisplayPipe('FishEye Video')
     original_display_pipe = DisplayPipe('Original Video')
 
     while True:
@@ -14,12 +20,16 @@ def video_processing_pipeline():
             break
 
         mirrored_frame = mirror_filter.process(frame)
+        fisheye_frame = fisheye_filter.process(frame)
 
-        if not original_display_pipe.display(frame) or not mirrored_display_pipe.display(mirrored_frame):
+        if not original_display_pipe.display(frame) \
+                or not mirrored_display_pipe.display(mirrored_frame) \
+                or not fisheye_display_pipe.display(fisheye_frame):
             break
 
     capture_pipe.release()
     cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
     video_processing_pipeline()
