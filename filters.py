abf0d864df90e6c7b85b8cc33ac29b4dfe32611e
Dmitry Zvidrin <dvzvidrin48@gmail.com>
2024-10-03 16:37:56 +0300

    FishEye Filter

--- filters.py
+++ filters.py
@@ -5,3 +5,11 @@ class MirrorFilterPipe:
     @staticmethod
     def process(frame):
         return cv2.flip(frame, 1)
+
+
+class FishEyeEffect:
+    @staticmethod
+    def process(frame):
+        return cv2.warpPolar(frame, (frame.shape[1], frame.shape[0]),
+                             (frame.shape[1] // 2, frame.shape[0] // 2),
+                             frame.shape[1] // 2, cv2.WARP_POLAR_LINEAR)
